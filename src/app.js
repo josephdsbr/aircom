b34af4b1580de47a870d3a2dbdb4e5f4456df58c
jose.vinicius <josephdsbr@gmail.com>
2019-12-09 18:34:48 -0300

    modificando e adicionando script de scheduling

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 import 'dotenv/config';
 
 import express from 'express';
+import { CronJob } from 'cron';
 import routes from './routes';
 import './database';
+
 
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.job();
   }
 
   middlewares() {
     this.server.use(express.json());
+  }
+
+  job () {
+    // const job = new CronJob('* * * * * *', () => {
+    //     console.log('generatin backg');
+    // }, null, true, 'America/Los_Angeles');
   }
 
   routes() {
