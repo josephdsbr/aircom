e7928e08bb9d556abd1b392c0e600fcdf7656351
josephdsbr <josephdsbr@gmail.com>
2019-12-09 23:14:41 -0300

    modificando horario do job para 30 minutos

--- src/job/index.js
+++ src/job/index.js
@@ -4,7 +4,7 @@ const schedules = [CheckinInform];
 
 function Schedules() {
   schedules.map(schedule => {
-    const job = new CronJob('* */30 * * * *', schedule.handle, null, true, 'America/Los_Angeles');
+    const job = new CronJob('* 30 * * * *', schedule.handle, null, true, 'America/Los_Angeles');
   });
 }
 
